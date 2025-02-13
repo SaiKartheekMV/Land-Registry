c0a7cf01856a7fd1aed17146cbe92ae97c889875
SaiKartheekMV <venkatasaikartheekmulukutla@gmail.com>
2025-02-13 22:57:05 +0530

    smartcontracts added

--- blockchain/contracts/LandRegistry.sol
+++ blockchain/contracts/LandRegistry.sol
@@ -1,23 +1,30 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.21;
 
-contract LandRegistry{
+contract LandRegistry {
     struct Land {
         uint id;
-        string owner;
+        address owner;
         string location;
         uint price;
     }
+
     mapping(uint => Land) public lands;
     uint public landCount = 0;
 
-    function registerLand(string memory _owner, string memory _location, uint _price) public {
+    event LandRegistered(uint id, address indexed owner, string location, uint price);
+
+    function registerLand(string memory _location, uint _price) public {
+        require(_price > 0, "Price must be greater than zero");
+
         landCount++;
-        lands[landCount] = Land(landCount, _owner, _location, _price);
+        lands[landCount] = Land(landCount, msg.sender, _location, _price);
+
+        emit LandRegistered(landCount, msg.sender, _location, _price);
     }
 
-    function getLand(uint _id) public view returns (Land memory){
+    function getLand(uint _id) public view returns (Land memory) {
+        require(_id > 0 && _id <= landCount, "Invalid land ID");
         return lands[_id];
     }
 }
