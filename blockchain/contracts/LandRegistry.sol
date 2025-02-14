761861facc787fcf1a360007507e74bb54acd325
SaiKartheekMV <venkatasaikartheekmulukutla@gmail.com>
2025-02-14 23:13:56 +0530

    changes to the smartcontracts

--- blockchain/contracts/LandRegistry.sol
+++ blockchain/contracts/LandRegistry.sol
@@ -10,21 +10,21 @@ contract LandRegistry {
     }
 
     mapping(uint => Land) public lands;
-    uint public landCount = 0;
+    uint public landCount;
 
-    event LandRegistered(uint id, address indexed owner, string location, uint price);
+    event LandRegistered(uint indexed id, address indexed owner, string location, uint price);
+
+    constructor() {
+        landCount = 0; // Initializing land count
+    }
 
     function registerLand(string memory _location, uint _price) public {
+        require(bytes(_location).length > 0, "Location cannot be empty");
         require(_price > 0, "Price must be greater than zero");
 
         landCount++;
         lands[landCount] = Land(landCount, msg.sender, _location, _price);
 
         emit LandRegistered(landCount, msg.sender, _location, _price);
-    }
-
-    function getLand(uint _id) public view returns (Land memory) {
-        require(_id > 0 && _id <= landCount, "Invalid land ID");
-        return lands[_id];
     }
 }
--- blockchain/truffle-config.js
+++ blockchain/truffle-config.js
@@ -1,11 +1,14 @@
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1", // Ganache running locally
-      port: 7545,        // Default Ganache port
-      network_id: "*",   // Match any network id (fixes your error)
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*",
+      gas: 8000000, // Increase this if needed
+      gasPrice: 20000000000  // 20 gwei
     },
   },
+  
   compilers: {
     solc: {
       version: "0.8.21", // Replace with your Solidity version
