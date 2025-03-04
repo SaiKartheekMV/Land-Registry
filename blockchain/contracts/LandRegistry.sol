fd65b8646bfb5f1ac24d2220f62eda0ec5f91e65
SaiKartheekMV <venkatasaikartheekmulukutla@gmail.com>
2025-03-05 01:52:29 +0530

    Trying to deploy the smartContract

--- blockchain/contracts/LandRegistry.sol
+++ blockchain/contracts/LandRegistry.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity ^0.8.28;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
 
 contract LandRegistry {
     struct Land {
@@ -16,9 +16,27 @@ contract LandRegistry {
     event LandRegistered(uint id, address owner, string location, uint price);
     event LandTransferred(uint id, address newOwner, uint price);
 
-    function registerLand(String memory _location, uint _price) public {
+    function registerLand(string memory _location, uint _price) public {
         landCount++;
         lands[landCount] = Land(landCount, msg.sender, _location, _price, false);
         emit LandRegistered(landCount, msg.sender, _location, _price);
+    }
+
+    function buyLand(uint _id) public payable {
+        require(lands[_id].forSale, "Land not for sale");
+        require(msg.value >= lands[_id].price, "Insufficient funds");
+
+        address previousOwner = lands[_id].owner;
+        lands[_id].owner = msg.sender;
+        lands[_id].forSale = false;
+
+        payable(previousOwner).transfer(msg.value);
+        emit LandTransferred(_id, msg.sender, msg.value);
+    }
+
+    function putLandForSale(uint _id, uint _price) public {
+        require(msg.sender == lands[_id].owner, "Not the owner");
+        lands[_id].price = _price;
+        lands[_id].forSale = true;
     }
 }
